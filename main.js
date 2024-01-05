22761a26eb450d97e742b27e025a0ce7f7202c97
esaint-dev <78765900+esaint-dev@users.noreply.github.com>
2024-01-05 22:20:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const btnEl = document.getElementById('get-btn')
+const formEL = document.getElementById('reg-form')
+
+btnEl.addEventListener('click', function(){
+    formEL.style.display = "block"
+})
